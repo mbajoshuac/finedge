0d4540fb8a686f7b4408e2941e23dc321c2bc0ae
Joshua Mba <angelicjoekeyz@gmail.com>
2020-12-16 16:53:01 +0100

    Feat: 1. complete the eslint configuration for the project

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const app = require('express')()
-const port = 8000 || 3000
+const app = require('express')();
 
+const port = 8000 || 3000;
 
-//Server port
+const ezez_live = '393030';
+
+const _id = 1234567890;
+
+const somefun = () => {
+  this._id = 2345;
+  const _thiiiii = 23456;
+};
+
+// Server port
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
