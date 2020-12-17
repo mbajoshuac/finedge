155d7dbd874c81a8271c33fb339d807b5a33b83d
Peter Okerulu <oduchep@gmail.com>
2020-12-17 08:32:48 +0100

    Break: actual App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import NavBarLogin from "../src/Components/NavBarLogin";
 import CreateAcccount from "../src/Components/CreateAccount";
-import SignIn from "../src/Components/SignIn";
 
 function App() {
   return (
-    <div className="App">
+    <iv className="App">
       <NavBarLogin/>
       <CreateAcccount />
-      <SignIn/>
-    </div>
+      
+    </iv>
   );
 }
 
