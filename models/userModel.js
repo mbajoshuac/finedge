46bb186dbe4b7886a516b29527d64d0b5ad1816e
Joshua Mba <angelicjoekeyz@gmail.com>
2020-12-17 10:10:28 +0100

    Break: Updated the model properties by adding a phone number

--- models/userModel.js
+++ models/userModel.js
@@ -3,18 +3,13 @@ const { Schema } = mongoose
 
 const usersSchema = new mongoose.Schema({
     firstName: String,
-<<<<<<< HEAD
     lastName: String,
+    phoneNumber: String,
     email: String,
     country: String,
-=======
-    lastName:String,
-    phoneNumber:Number,
-    email: String,
-    country : String,
->>>>>>> a864cf0c405e04641fd30f64edf7e23751838adf
     password: String,
     photo: String
+
 })
 
 module.exports = mongoose.model(user, usersSchema)
