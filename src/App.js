c6a831ec8c7ba6a50a167ade75bef88121b4f675
Peter Okerulu <oduchep@gmail.com>
2020-12-17 08:16:56 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import NavBarLogin from "../src/Components/NavBarLogin";
+// import NavBarLogin from "../src/Components/NavBarLogin";
+import SignIn from "../src/Components/SignIn";
+
 
 function App() {
   return (
     <div className="App">
-      <NavBarLogin/>
+      {/* <NavBarLogin/> */}
+      <SignIn/>
     </div>
   );
 }
