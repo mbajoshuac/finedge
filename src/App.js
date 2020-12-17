95a7354042456451690bf38bb359c1649770d250
Eagle <eobumma@gmail.com>
2020-12-17 04:03:13 +0100

    add create account page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBarLogin from "../src/Components/NavBarLogin";
+import CreateAcccount from "../src/Components/CreateAccount";
 
 function App() {
   return (
     <div className="App">
       <NavBarLogin/>
+      <CreateAcccount />
     </div>
   );
 }
---
+++ src/Components/CreateAccount.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./CreateAccount.css";
+
+function CreateAccount() {
+    return (
+        <>
+            <section className="create-account">
+                <h1>Create Your Account</h1>
+                <div className="account-form">
+                    <form>
+                        <div className="form-control flex">
+                            <input type="text" placeholder="First Name" />
+                            <input type="text" placeholder="Last Name" />
+                        </div>
+                        <div className="form-control">
+                            <input type="password" placeholder="Choose password" />
+                        </div>
+                        <div className="form-control">
+                            <input type="checkbox"/><label> I clerify that I am 13 years of age or older, and agree to the <span>User Agreement</span> and <span>Privacy Policy</span>.</label>
+                        </div>
+                        <div className="form-control">
+                            <button type="submit" className="bttn">Sign Up</button>
+                        </div>
+                    </form>
+                    <p>Already registered? or <span>Log in</span></p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CreateAccount;
